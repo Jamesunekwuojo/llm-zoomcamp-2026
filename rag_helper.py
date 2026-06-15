77a904cf261647d9b2f664175e7e8f1b5e083730
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2026-06-15 05:48:16 +0100

    feat:refactored rag_helper..

--- rag_helper.py
+++ rag_helper.py
@@ -17,6 +17,8 @@ CONTEXT:
 
 class RAGBase:
 
+    MODEL = "openai/gpt-oss-20b"
+
     def __init__(
         self,
         index,
@@ -24,7 +26,7 @@ class RAGBase:
         instructions=INSTRUCTIONS,
         prompt_template=PROMPT_TEMPLATE,
         course="llm-zoomcamp",
-        model="gpt-5.4-mini"
+        model=MODEL
     ):
         self.index = index
         self.llm_client = llm_client
@@ -32,3 +34,55 @@ class RAGBase:
         self.course = course
         self.prompt_template = prompt_template
         self.model = model
+    
+    # The search method delegates to the index:
+    def search(self, query, num_results=5):
+        boost_dict = {"question": 3.0, "section": 0.5}
+        filter_dict = {"course": self.course}
+
+        return self.index.search(
+            query,
+            num_results=num_results,
+            boost_dict=boost_dict,
+            filter_dict=filter_dict
+        )
+    
+    # The build context method takes the search results and builds the context.
+    def build_context(self, search_results):
+        lines = []
+
+        for doc in search_results:
+            lines.append(doc["section"])
+            lines.append("Q: " + doc["question"])
+            lines.append("A: " + doc["answer"])
+            lines.append("")
+
+        return "\n".join(lines).strip()
+
+    def build_prompt(self, query, search_results):
+        context = self.build_context(search_results)
+        return self.prompt_template.format(
+            question=query, context=context
+        )
+    
+
+    # the llm method sends the prompt to the LLM
+    def llm(self, prompt):
+        input_messages = [
+            {"role": "developer", "content": self.instructions},
+            {"role": "user", "content": prompt}
+        ]
+
+        response = self.llm_client.chat.completions.create(
+            model=self.model,
+            messages=input_messages
+        )
+
+        return response.choices[0].message.content
+    
+    # The rag method ties everything together:
+    def rag(self, query):
+        search_results = self.search(query)
+        prompt = self.build_prompt(query, search_results)
+        answer = self.llm(prompt)
+        return answer
